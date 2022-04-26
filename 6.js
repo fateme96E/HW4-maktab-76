7335bd3fa75e68557d359eb5ac4ff0bd37519221
fateme96E <92683438+fateme96E@users.noreply.github.com>
2022-04-26 22:47:10 +0430

    Update q6

--- 6.js
+++ 6.js
@@ -1,11 +1,5 @@
 function sumRoundNums(arr){
-    let sum = arr.reduce((acc, item) =>{
-        console.log(acc, item);
-         return acc + Math.round(item);
-        
-        
-        
-    });
+    let sum = arr.reduce((acc, item) => acc + Math.round(item), 0);
     console.log(sum);
 }
 sumRoundNums([15.5, 2.3, 1.1, 4.7]);
